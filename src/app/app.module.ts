43c9eb298c9e0e381accaa6a023493e9d2341bf3
Marco Enrico Piras <kikkomep@crs4.it>
2020-04-15 09:41:51 +0200

    Add service to support runtime settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppConfigService } from './providers/app-config.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+export function initConfig(appConfig: AppConfigService) {
+  return () => appConfig.loadConfig();
+}
 
 @NgModule({
   declarations: [
@@ -20,7 +23,12 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserAnimationsModule
   ],
   
-  providers: [],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: initConfig,
+    deps: [AppConfigService],
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
