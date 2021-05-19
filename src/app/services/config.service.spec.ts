3548162fb754d25af45bee09bcb315b9366e6a19
Marco Enrico Piras <kikkomep@crs4.it>
2021-05-19 09:38:11 +0000

    Add minimal ts.spec for the AppConfigService

---
+++ src/app/services/config.service.spec.ts
@@ -0,0 +1,11 @@
+import { AppConfigService } from './config.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('AppConfigService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: AppConfigService = TestBed.get(AppConfigService);
+    expect(service).toBeTruthy();
+  });
+});
